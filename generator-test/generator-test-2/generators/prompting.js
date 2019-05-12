95a8285df23ab49e50dd782f1ad07f49cdff185d
Arunkumar <arunkumarcoderelm@gmail.com>
2019-05-12 07:26:40 +0530

    code optimize

--- generator-test/generator-test-2/generators/prompting.js
+++ generator-test/generator-test-2/generators/prompting.js
@@ -3,6 +3,44 @@ const chalk = require("chalk");
 
 module.exports = async generator => {
   let props = {};
+  const interact = [
+    {
+      type: "input",
+      name: "name",
+      message: "Provide Application Name:",
+      default: generator.appname
+    },
+    {
+      type: "input",
+      name: "version",
+      message: "Provide Application Version:",
+      default: "1.0.0"
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide Application Description:",
+      default: "Coderelm Express MVC Setup."
+    },
+    {
+      type: "input",
+      name: "repository",
+      message: "Provide Application Repository:",
+      default: ""
+    },
+    {
+      type: "input",
+      name: "author",
+      message: "Provide Application Author 'nanem <email@gmail.com>':",
+      default: ""
+    },
+    {
+      type: "input",
+      name: "license",
+      message: "Provide Application License:",
+      default: "MIT"
+    }
+  ];
 
   // Have Yeoman greet the user.
   generator.log(
@@ -12,74 +50,6 @@ module.exports = async generator => {
       )} generator!`
     )
   );
-
-  const name = [
-    {
-      type: "input",
-      name: "name",
-      message: "Provide Application Name:",
-      default: generator.appname
-    }
-  ];
-
-  const version = [
-    {
-      type: "input",
-      name: "version",
-      message: "Provide Application Version:",
-      default: "1.0.0"
-    }
-  ];
-
-  const description = [
-    {
-      type: "input",
-      name: "description",
-      message: "Provide Application Description:",
-      default: "Coderelm Express MVC Setup."
-    }
-  ];
-
-  const repository = [
-    {
-      type: "input",
-      name: "repository",
-      message: "Provide Application Repository:",
-      default: ""
-    }
-  ];
-
-  const author = [
-    {
-      type: "input",
-      name: "author",
-      message: "Provide Application Author 'nanem <email@gmail.com>':",
-      default: ""
-    }
-  ];
-
-  const license = [
-    {
-      type: "input",
-      name: "license",
-      message: "Provide Application License:",
-      default: "MIT"
-    }
-  ];
-
-  props.name = await generator.prompt(name);
-  props.version = await generator.prompt(version);
-  props.description = await generator.prompt(description);
-  props.repository = await generator.prompt(repository);
-  props.author = await generator.prompt(author);
-  props.license = await generator.prompt(license);
-
-  return {
-    name: props.name.name,
-    version: props.version.version,
-    description: props.description.description,
-    repository: props.repository.repository,
-    author: props.author.author,
-    license: props.license.license
-  };
+  props = await generator.prompt(interact);
+  return props;
 };
--- generator-test/generator-test-2/generators/writing.js
+++ generator-test/generator-test-2/generators/writing.js
@@ -1,43 +1,60 @@
 const _ = require("lodash");
 
-module.exports = async generator => {
-  let project = generator.props;
+class Writeing {
 
-  /**
-   * Copying default files and folders.
-   */
-  generator.fs.copy(
-    generator.sourceRoot() + "/default",
-    generator.destinationRoot()
-  );
+  // Copying default files and folders.
+  static folders(generator) {
+    generator.fs.copy(
+      generator.sourceRoot() + "/default",
+      generator.destinationRoot()
+    );
+  }
 
   // Package.json
-  generator.fs.copyTpl(
-    generator.templatePath("package.json"),
-    generator.destinationPath("package.json"),
-    {
-      projectName: _.kebabCase(project.name),
-      projectVersion: project.version,
-      projectDescription: project.description,
-      projectRepository: project.repository,
-      projectAuthor: project.author,
-      projectLicense: project.license
-    }
-  );
+  static packageJson(generator) {
+    let project = generator.props;
+    generator.fs.copyTpl(
+      generator.templatePath("package.json"),
+      generator.destinationPath("package.json"),
+      {
+        projectName: _.kebabCase(project.name),
+        projectVersion: project.version,
+        projectDescription: project.description,
+        projectRepository: project.repository,
+        projectAuthor: project.author,
+        projectLicense: project.license
+      }
+    );
+  }
+
   // Ecosystem.config.js
-  generator.fs.copyTpl(
-    generator.templatePath("ecosystem.config.js"),
-    generator.destinationPath("ecosystem.config.js"),
-    { projectName: _.kebabCase(project.name) }
-  );
+  static pm2Ecosystem(generator) {
+    let project = generator.props;
+    generator.fs.copyTpl(
+      generator.templatePath("ecosystem.config.js"),
+      generator.destinationPath("ecosystem.config.js"),
+      { projectName: _.kebabCase(project.name) }
+    );
+  }
+
   // Config/config.js
-  generator.fs.copyTpl(
-    generator.templatePath() + "/config/default.json",
-    generator.destinationPath() + "/config/default.json",
-    {
-      appSecret: "dsfsdfgdgdf",
-      storageSecret: "34675867uthgfdfefrerg",
-      sessionStorageSecret: "regnmj756y5t4reqw"
-    }
-  );
+  static config(generator) {  
+    let project = generator.props;
+    generator.fs.copyTpl(
+      generator.templatePath() + "/config/default.json",
+      generator.destinationPath() + "/config/default.json",
+      {
+        appSecret: "dsfsdfgdgdf",
+        storageSecret: "34675867uthgfdfefrerg",
+        sessionStorageSecret: "regnmj756y5t4reqw"
+      }
+    );
+  }
+}
+
+module.exports = async generator => {
+  Writeing.folders(generator)
+  Writeing.packageJson(generator)
+  Writeing.pm2Ecosystem(generator)
+  Writeing.config(generator)
 };
